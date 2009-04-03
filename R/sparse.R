001bceaa7f6ff76264daaabffcf8cb294030dcf1
Jorn Todling <toedling@ebi.ac.uk>
2009-04-03 09:37:36 +0000

    removed obsolete method for no longer used class matrix.csr
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Ringo@38460 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/sparse.R
+++ R/sparse.R
@@ -6,20 +6,6 @@ setMethod("nonzero", signature(x="matrix"), function(x){
     return(which(x != 0, arr.ind=TRUE))
 })
 
-setMethod("nonzero", signature(x="matrix.csr"), function(x){
-  ## function to get a two-column matrix containing the indices of the
-  ### non-zero elements in a "matrix.csr" class matrix
-  if (all(x@ra==0))
-      return(matrix(0, nrow=0, ncol=2, dimnames=
-                    list(character(0), c("row","col"))))
-  res <- cbind(rep(seq(dim(x)[1]),diff(x@ia)), # row indices
-               x@ja )# column indices directly saved in matrix.csr
-  colnames(res) <- c("row","col")
-  ## remove zero elements
-  res <- res[x@ra != 0,,drop=FALSE]
-  return(res)
-})
-
 setMethod("nonzero", signature(x="dgCMatrix"), function(x){
     ## function to get a two-column matrix containing the indices of the
     ### non-zero elements in a "dgCMatrix" class matrix
