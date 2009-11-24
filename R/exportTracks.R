2275b93d85b25b36bcc6e72118768a1ef5e28397
Jorn Todling <toedling@ebi.ac.uk>
2009-11-24 13:50:25 +0000

    fixed exportCherList function; this fix became necessary because rtracklayer does not contain the trackSet class any longer
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Ringo@43232 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/exportTracks.R
+++ R/exportTracks.R
@@ -1,5 +1,5 @@
 
-exportCherList <- function(object, filename="chers.gff", format="gff3",...)
+exportCherList <- function(object, filename="chers.gff", format="gff3", genome="hg18", ...)
 {
   require("rtracklayer")
   stopifnot(inherits(object, "cherList"))
@@ -8,14 +8,20 @@ exportCherList <- function(object, filename="chers.gff", format="gff3",...)
   chersDf$chrom <- paste("chr", sub("^chr", "", chersDf$chr), sep = "")
   chersDf$start <- as.integer(as.character(chersDf$start))
   chersDf$end <- as.integer(as.character(chersDf$end))
-  chersAdf <- as(chersDf, "AnnotatedDataFrame")
-  descs <- c(chrom = "Chromosome ID",
-             start = "Start position on chromosome",
-             end = "End position on chromosome",
-             strand = "DNA strand, sense (+) or antisense (-)")
-  md <- data.frame(labelDescription = descs, stringsAsFactors = FALSE)
-  varMetadata(chersAdf)[rownames(md),] <- md
-  chersTS <- rtracklayer:::trackSet(chersAdf, ...)
-  export(chersTS, filename, format=format)
+  cherRanges <- IRanges(chersDf$start, chersDf$end)
+  cherTrack  <- GenomicData(cherRanges,
+                            chersDf[, c("strand", "name", "antibody")],
+                            chrom = chersDf$chrom,
+                            genome = genome)
+  ## old stuff, does not work with rtracklayer >= 1.7.0
+  #chersAdf <- as(chersDf, "AnnotatedDataFrame")
+  #descs <- c(chrom = "Chromosome ID",
+  #           start = "Start position on chromosome",
+  #           end = "End position on chromosome",
+  #           strand = "DNA strand, sense (+) or antisense (-)")
+  #md <- data.frame(labelDescription = descs, stringsAsFactors = FALSE)
+  #varMetadata(chersAdf)[rownames(md),] <- md
+  #cherTrack <- rtracklayer:::trackSet(chersAdf, ...)
+  export(cherTrack, filename, format=format)
   invisible(NULL)
 }# exportCherList
